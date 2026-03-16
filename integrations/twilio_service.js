3ee202bffde9d909e2d490e9bbcff92d1d929ab6
m1n1v1rus <ayushmani163@gmail.com>
2026-03-17 03:19:14 +0530

    feat: add Twilio integration for WhatsApp notifications

---
+++ integrations/twilio_service.js
@@ -0,0 +1,8 @@
+// Twilio placeholder layout
+export const sendSMS = async (to, body) => {
+  console.log(`Sending SMS to ${to}: ${body}`);
+};
+
+export const sendWhatsApp = async (to, body) => {
+  console.log(`Sending WhatsApp to ${to}: ${body}`);
+};
